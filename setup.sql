d6a7483d71aae56bf7b1f36fb0b9c6a4b4d0d889
Cristian Prochnow <cristianprochnow@gmail.com>
2023-06-27 11:08:11 -0300

    refactor: reorganize folders

--- setup.sql
+++ setup.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE IMOBI;
 USE IMOBI;
 
-source /app/create.sql;
-source /app/alter_table.sql;
-source /app/procedures.sql;
-source /app/view.sql;
-source /app/trigger.sql;
-source /app/call_procedures_insert.sql;
+source /app/src/create.sql;
+source /app/src/alter_table.sql;
+source /app/src/procedures.sql;
+source /app/src/view.sql;
+source /app/src/trigger.sql;
+source /app/src/call_procedures_insert.sql;
